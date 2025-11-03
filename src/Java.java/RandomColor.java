fcb6e60e3d29ad3288aceceec02e227564dcc54b
Oliver Bender <28Obender@student.penfield.edu>
2025-11-03 13:12:37 -0500

    added random brightness and while loop
    i think i did smth wrong with the code just under main with the instances but idk, added while loop and still have to add isbuttonpressed but its coming along

--- src/Java.java/RandomColor.java
+++ src/Java.java/RandomColor.java
@@ -1,22 +1,42 @@
 import java.util.Random;
 
-public class RandomNumberGenerator {
-        Random random = new Random();
-        public int randomNumber () {
+bool true = true;
+
+public class RandomColorGenerator {
+        Random random = new RandomColor();
+        public int randomColorNumber () {
                 random.nextInt(4) + 1; 
         }
     }
 
+public class RandomBrightnessGenerator {
+        Random random = new RandomBrightness();
+        public int randomBrightnessNumber () {
+                random.nextInt(2) + 1; 
+        }
+    }
+
 public static void main() {
-    RandomNumberGenerator = new RandomNumberGenerator(); 
-    randomNumber = generator.random();
-        if (randomNumber == 1) {
-            System.out.println ("System.setcolor (255, 0, 0);");
-        } else if (randomNumber == 2) {
-            System.out.println ("System.setcolor (255, 90, 0);");
-        } else if (randomNumber == 3) {
-            System.out.println ("System.setcolor (255, 232, 8);");
-        } else {
-            System.out.println ("System.setcolor (170, 66, 3);");
+        RandomNumberGenerator = new RandomNumberGenerator(); 
+        randomNumber = generator.RandomColor();
+        RandomBrightnessGenerator = new RandomColorGenerator();
+        RandomBrightness = generator.RandomBrightness();
+        while (true == true) {
+                 if (randomColorNumber == 1) {
+                    System.out.println ("System.setcolor (255, 0, 0);");
+                } else if (randomColorNumber == 2) {
+                    System.out.println ("System.setcolor (255, 90, 0);");
+                } else if (randomColorNumber == 3) {
+                    System.out.println ("System.setcolor (255, 232, 8);");
+                } else {
+                    System.out.println ("System.setcolor (170, 66, 3);");
+                }
+                if (randomBrightnessNumber == 1) {
+                        System.out.println ("System.setbrightness (255);")                 
+                } else {
+                        System.out.println ("System.setbrightness (190)")
+                }
+                delay (25);
+                
         }
 }
