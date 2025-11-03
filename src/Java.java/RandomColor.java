8577289950555bbfbdfe6a115621f319f9ce6799
Oliver Bender <28Obender@student.penfield.edu>
2025-11-03 10:51:58 -0500

    put if statement in main instead of the method

--- src/Java.java/RandomColor.java
+++ src/Java.java/RandomColor.java
@@ -3,8 +3,11 @@ import java.util.Random;
 public class RandomNumberGenerator {
         Random random = new Random();
         int randomNumber = random.nextInt(4) + 1; 
+    }
 
-        if (randomNumber == 1) {
+public static void main() {
+    RandomNumberGenerator.staticMethod(); //should i make it return something here for the if statement? or should i have it return something in the method?
+                if (randomNumber == 1) {
             System.out.println ("System.setcolor (255, 0, 0);");
         } else if (randomNumber == 2) {
             System.out.println ("System.setcolor (255, 90, 0);");
@@ -13,8 +16,4 @@ public class RandomNumberGenerator {
         } else if (randomNumber == 4) {
             System.out.println ("System.setcolor (170, 66, 3);");
         }
-    }
-
-public static void main() {
-    RandomNumberGenerator.staticMethod();
 }
